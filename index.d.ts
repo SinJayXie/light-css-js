29dbbb12e37eea0af2bc5faa644ca6591acdf0d7
sinjayxie <sinjayxie@gmail.com>
2026-01-30 10:02:03 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -4,52 +4,9 @@ declare interface IRule {
 }
 
 export declare class LightCSS {
-    private readonly ob;
-    private readonly classMap;
-    private readonly style;
-    private readonly rules;
-    private readonly parentClass;
-    private readonly insertMode;
-    private readonly cacheClassName;
-    private readonly config;
-    private sheet;
-    private lastUpdateTime;
     version: string;
     constructor(opt: LightCSSOptions);
-    private addCache;
-    /**
-     * 处理新添加的元素节点
-     * @param addedNodes
-     * @private
-     */
-    private processAddedNodes;
-    /**
-     * 处理 class 属性的变动
-     * @param mutation
-     * @private
-     */
-    private processClassPatch;
-    /**
-     * 创建样式
-     * @private
-     */
-    private createStyle;
-    /**
-     * 监听 MutationObserver
-     * @param mutations
-     * @private
-     */
-    private handler;
-    /**
-     * 销毁实例
-     */
     destroy(): void;
-    /**
-     * 初始化
-     * @param isAppendDefault
-     * @private
-     */
-    private init;
 }
 
 export declare function lightCSS(opt?: LightCSSOptions): LightCSS;
