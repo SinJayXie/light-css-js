9ad640720a74c48865cb327a7962903697cbd2b3
sinjayxie <sinjayxie@gmail.com>
2026-01-30 09:36:39 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     include: ['lib/**/*']
   })],
   build: {
-    sourcemap: true,
+    sourcemap: false,
     minify: 'terser',
     terserOptions: {
       mangle: {
