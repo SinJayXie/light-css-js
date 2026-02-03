bf2d6a30c4af373f2b674593ba2becadc3c6c63e
sinjayxie <sinjayxie@gmail.com>
2026-02-03 16:02:13 +0800

    update

---
+++ patch-version.js
@@ -0,0 +1,10 @@
+import * as fs from 'fs';
+import packageJson from './package.json' with {type: 'json'};
+
+const coreScript = fs.readFileSync('./lib/core.ts', 'utf8').toString();
+const regex = /const VERSION = '(.*)';/;
+const versionTemplate = `const VERSION = '${packageJson.version}'`;
+const replaceText = coreScript.match(regex)[0];
+const bufferStr = coreScript.replace(replaceText, versionTemplate);
+
+fs.writeFileSync('./lib/core.ts', bufferStr);
