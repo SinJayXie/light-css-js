19bfcc94ffcf1f466795e0ba10e307492a9ec5e4
sinjayxie <sinjayxie@gmail.com>
2026-02-05 15:25:05 +0800

    Optimize code performance

--- index.d.ts
+++ index.d.ts
@@ -5,52 +5,9 @@ declare interface IRule {
 }
 
 export declare class LightCSS {
-    private readonly ob;
-    private readonly classMap;
-    private readonly style;
-    private readonly rules;
-    private readonly parentClass;
-    private readonly insertMode;
-    private readonly cacheClassName;
-    private readonly config;
-    private sheet;
-    private lastUpdateTime;
     version: string;
     constructor(opt: LightCSSOptions);
-    private addCache;
-    /**
-     * Process newly added element nodes
-     * @param addedNodes List of nodes added to the DOM
-     * @private
-     */
-    private processAddedNodes;
-    /**
-     * Process changes to the class attribute
-     * @param mutation MutationRecord containing class attribute change info
-     * @private
-     */
-    private processClassPatch;
-    /**
-     * Generate styles from cached class names
-     * @private
-     */
-    private createStyle;
-    /**
-     * Handle MutationObserver events
-     * @param mutations Array of MutationRecords from the observer
-     * @private
-     */
-    private handler;
-    /**
-     * Destroy the LightCSS instance and clean up resources
-     */
     destroy(): void;
-    /**
-     * Initialize the LightCSS instance
-     * @param isAppendDefault Whether to include default style rules
-     * @private
-     */
-    private init;
 }
 
 export declare function lightCSS(opt?: LightCSSOptions): LightCSS;
